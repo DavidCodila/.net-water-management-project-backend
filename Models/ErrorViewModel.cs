3fae3bd19276302efdcd2bd7cbf77870ebc91792
david <Davids-MacBook-Pro.local>
2024-04-30 11:21:50 +1000

    Initalised project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace water_management_project_backend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
